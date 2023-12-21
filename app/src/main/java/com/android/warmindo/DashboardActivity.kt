a2f14d4e2ff638e308eb32c13fa83b811058fcef
SHieon <89286782+phantomeon@users.noreply.github.com>
2023-12-21 22:54:46 +0700

    Penambahan Aktivitas_Pengguna

--- app/src/main/java/com/android/warmindo/DashboardActivity.kt
+++ app/src/main/java/com/android/warmindo/DashboardActivity.kt
@@ -7,6 +7,10 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.database.FirebaseDatabase
+import java.text.DateFormat
+import java.util.Date
+import java.util.Locale
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -16,14 +20,17 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
-        welcomeText = findViewById(R.id.welcome_text)
-
         // Ambil data dari Intent yang dikirim dari aktivitas sebelumnya (MainActivity)
-        val namaPengguna = intent.getStringExtra("namapengguna")
-        val idRole = intent.getIntExtra("idrole", 0)
         val mulaiShiftButton: Button = findViewById(R.id.mulai_shift_button)
         val logoutButton: Button = findViewById(R.id.logout_button)
+        val namaPengguna = intent.getStringExtra("namapengguna")
+        val idPengguna = intent.getStringExtra("idpengguna")
+        val idRole = intent.getIntExtra("idrole", 0)
+        // Variabels untuk menambahkan aktivitas pengguna
+        val databaseAct = FirebaseDatabase.getInstance().getReference("aktivitas_pengguna")
+        val idaktivitas = databaseAct.push().key // Menghasilkan kunci unik
 
+        welcomeText = findViewById(R.id.welcome_text)
         // Periksa apakah namaPengguna tidak null
         if (namaPengguna != null) {
             Log.d("DashboardActivity", "Masuk ke blok if")
@@ -40,10 +47,30 @@ class DashboardActivity : AppCompatActivity() {
         mulaiShiftButton.setOnClickListener {
             if (idRole == 4) {
                 Toast.makeText(applicationContext, "Selamat Bekerja, Petugas Dapur $namaPengguna", Toast.LENGTH_SHORT).show()
-                val intent = Intent(applicationContext, TransactionActivity::class.java)
-                startActivity(intent)
-                // Menutup aktivitas DashboardActivity
-                finish()
+                if (idaktivitas != null) {
+                    // Dapatkan tanggal dan waktu saat ini
+                    val currentDateTime = Date()
+                    // Dapatkan formatter untuk tanggal dan waktu dengan format lokal
+                    val dateFormatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault())
+                    val timeFormatter = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault())
+                    // Format tanggal dan waktu
+                    val formattedDate = dateFormatter.format(currentDateTime)
+                    val formattedTime = timeFormatter.format(currentDateTime)
+
+                    // Intent ke TransactionActivity
+                    val intent = Intent(applicationContext, TransactionActivity::class.java)
+                    startActivity(intent)
+
+                    //Menambahkan data ke tabel aktivitas_pengguna
+                    databaseAct.child(idaktivitas).child("aktivitas").setValue("AKSES SHIFT")
+                    databaseAct.child(idaktivitas).child("idpengguna").setValue(idPengguna)
+                    databaseAct.child(idaktivitas).child("idaktivitas").setValue(idaktivitas)
+                    databaseAct.child(idaktivitas).child("tanggal").setValue(formattedDate)
+                    databaseAct.child(idaktivitas).child("waktu").setValue(formattedTime)
+
+                    // Menutup aktivitas DashboardActivity
+                    finish()
+                }
             } else if (idRole == 2) {
                 Toast.makeText(applicationContext, "Selamat Bekerja, Petugas Kasir $namaPengguna", Toast.LENGTH_SHORT).show()
             } else if (idRole==3) {
@@ -55,10 +82,30 @@ class DashboardActivity : AppCompatActivity() {
         logoutButton.setOnClickListener {
             // Kode logout di sini, misalnya menghapus data sesi atau membuka halaman login
             Toast.makeText(applicationContext, "Sedang Dalam Proses Logout..", Toast.LENGTH_SHORT).show()
-            val intent = Intent(applicationContext, MainActivity::class.java)
-            startActivity(intent)
-            // Menutup aktivitas DashboardActivity
-            finish()
+            if (idaktivitas != null) {
+                // Dapatkan tanggal dan waktu saat ini
+                val currentDateTime = Date()
+                // Dapatkan formatter untuk tanggal dan waktu dengan format lokal
+                val dateFormatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault())
+                val timeFormatter = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault())
+                // Format tanggal dan waktu
+                val formattedDate = dateFormatter.format(currentDateTime)
+                val formattedTime = timeFormatter.format(currentDateTime)
+
+                // Intent ke MainActivity
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+
+                //Menambahkan data ke tabel aktivitas_pengguna
+                databaseAct.child(idaktivitas).child("aktivitas").setValue("LOGOUT")
+                databaseAct.child(idaktivitas).child("idpengguna").setValue(idPengguna)
+                databaseAct.child(idaktivitas).child("idaktivitas").setValue(idaktivitas)
+                databaseAct.child(idaktivitas).child("tanggal").setValue(formattedDate)
+                databaseAct.child(idaktivitas).child("waktu").setValue(formattedTime)
+
+                // Menutup aktivitas DashboardActivity
+                finish()
+            }
             Toast.makeText(applicationContext, "Logout Berhasil", Toast.LENGTH_SHORT).show()
         }
     }
--- app/src/main/java/com/android/warmindo/MainActivity.kt
+++ app/src/main/java/com/android/warmindo/MainActivity.kt
@@ -2,7 +2,6 @@ package com.android.warmindo
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -12,7 +11,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-
+import java.text.DateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var passwordEditText: EditText
     private lateinit var loginButton: Button
     private lateinit var database: DatabaseReference
+    private lateinit var databaseAct: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,30 +35,61 @@ class MainActivity : AppCompatActivity() {
             val username: String = usernameEditText.text.toString()
             val password: String = passwordEditText.text.toString()
             database = FirebaseDatabase.getInstance().getReference("pengguna")
+            databaseAct = FirebaseDatabase.getInstance().getReference("aktivitas_pengguna")
             if (username.isEmpty() || password.isEmpty()) {
                 Toast.makeText(applicationContext, "Username Atau Password Belum Terisi", Toast.LENGTH_SHORT).show()
             } else {
                 database.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object : ValueEventListener {
                         override fun onDataChange(snapshot: DataSnapshot) {
                             if (snapshot.exists()) {
-                                // User dengan username tersebut ditemukan, dilakukan pengecekan password
+                                // User dengan username tersebut ditemukan
                                 for (userSnapshot in snapshot.children) {
 
+                                    // Variabels untuk menampung atribut dari data dengan username terkait
+                                    val idPengguna = userSnapshot.child("idpengguna").getValue(String::class.java)
                                     val storedPassword = userSnapshot.child("password").getValue(String::class.java)
-                                    val status = userSnapshot.child("status").getValue(String::class.java)
+                                    val namaPengguna = userSnapshot.child("namapengguna").getValue(String::class.java)
                                     val idRole = userSnapshot.child("idrole").getValue(Int::class.java)
+                                    val status = userSnapshot.child("status").getValue(String::class.java)
 
+                                    // Pengecekan password, status, dan idrole
                                     if (storedPassword == password && status == "AKTIF" && (idRole == 2 || idRole == 3 || idRole == 4)) {
+                                        Toast.makeText(applicationContext, "Sedang Menghubungkan Pengguna...", Toast.LENGTH_SHORT).show()
+
+                                        val idaktivitas = databaseAct.push().key // Menggunakan push().key untuk mendapatkan ID baru secara otomatis
+                                        if (idaktivitas != null) {
+                                            // Dapatkan tanggal dan waktu saat ini
+                                            val currentDateTime = Date()
+                                            // Dapatkan formatter untuk tanggal dan waktu dengan format lokal
+                                            val dateFormatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault())
+                                            val timeFormatter = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault())
+                                            // Format tanggal dan waktu
+                                            val formattedDate = dateFormatter.format(currentDateTime)
+                                            val formattedTime = timeFormatter.format(currentDateTime)
+
+                                            // Intent data ke page setelahnya
+                                            val intent = Intent(applicationContext, DashboardActivity::class.java)
+                                            intent.putExtra("namapengguna", namaPengguna)
+                                            intent.putExtra("idrole", idRole)
+                                            intent.putExtra("idpengguna", idPengguna)
+                                            startActivity(intent)
+
+                                            // Menambahkan aktivitas Login untuk pengguna terkait ke aktivitas_pengguna
+                                            databaseAct.child(idaktivitas).child("aktivitas").setValue("LOGIN")
+                                            databaseAct.child(idaktivitas).child("idpengguna").setValue(idPengguna)
+                                            databaseAct.child(idaktivitas).child("idaktivitas").setValue(idaktivitas)
+                                            databaseAct.child(idaktivitas).child("tanggal").setValue(formattedDate)
+                                            databaseAct.child(idaktivitas).child("waktu").setValue(formattedTime)
+                                        } else {
+                                            Toast.makeText(applicationContext, "Gagal Menambahkan Log Aktivitas, Harap Hubungi Pemilik", Toast.LENGTH_SHORT).show()
+                                        }
+
                                         Toast.makeText(applicationContext, "Login Berhasil", Toast.LENGTH_SHORT).show()
-                                        val intent = Intent(applicationContext, DashboardActivity::class.java)
-                                        intent.putExtra("namapengguna", userSnapshot.child("namapengguna").getValue(String::class.java))
-                                        intent.putExtra("idrole", idRole)
-                                        startActivity(intent)
                                     } else {
                                         if (status != "AKTIF") {
                                             Toast.makeText(applicationContext, "Akun Tidak Aktif", Toast.LENGTH_SHORT).show()
                                         } else if (idRole == 1) {
-                                            Toast.makeText(applicationContext, "Mengarahkan Pengguna ke Halaman Dashboard Pemilik..", Toast.LENGTH_SHORT).show()
+                                            Toast.makeText(applicationContext, "Mengarahkan Pengguna ke Halaman Dashboard Pemilik...", Toast.LENGTH_SHORT).show()
                                         } else if (idRole != 1) {
                                             Toast.makeText(applicationContext, "Akun Tidak Memiliki Hak Akses", Toast.LENGTH_SHORT).show()
                                         }else {
